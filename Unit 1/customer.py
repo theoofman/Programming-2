19f4388d59cf24d67fce991e2dbac4262e978bc9
theoofman <tyler@elsoutlet.com>
2022-02-15 08:53:46 -0800

    a

--- Unit 1/customer.py
+++ Unit 1/customer.py
@@ -1,16 +1,18 @@
 myFile = open("data.txt","r")
 customers = myFile.readlines()
-choice = input("Would you like to look up a customer by line number or last name?")
+myFile.close()
+print("stop")
+choice = input("Would you like to look up a customer by line number or last name? >")
 while choice.lower() not in ["line number", "line", "name", "last name"]:
     choice = input("Sorry, that's not an option. Type 'line number' or 'name'")
 
 if choice.lower() in ["line number", "line"]:
-    line = int(input("Please enter the line number: "))
+    line = int(input("Please enter the line number: >"))
     print(customers[line - 1])
 
 found = False
 if choice.lower() in ["name", "last name"]:
-    name = input("Please enter the name: ")
+    name = input("Please enter the name: >")
     for line in customers:
         if name in line:
             print("The customer's full name is", line)
